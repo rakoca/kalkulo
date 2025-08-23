40100bce31ef8a80b019077985a8e4fa15bc9e5a
Kuba Rakoca <jakub.rakoca@int.pl>
2025-08-23 20:48:49 +0200

    Test połączenia z remote

---
+++ README.md
@@ -0,0 +1,2 @@
+# KalkULO
+Strona internetowa, która zawiera przydatne narzędzia dla licealisty, stworzona przez ucznia ULO szczególnie dla społeczności tej szkoły
---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('test').addEventListener('click', () => {
+    let bee = document.getElementById('bee');
+    bee.style.display == 'none' ? bee.style.display = 'block' : bee.style.display = 'none';
+});
